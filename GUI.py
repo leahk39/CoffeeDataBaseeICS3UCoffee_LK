f28db94efbaabb7a4ee4cd7d4a31365ea3f72c91
leahk39 <leahk39@kprschools.ca>
2026-06-12 14:27:25 -0400

    GUI

---
+++ GUI.py
@@ -0,0 +1,34 @@
+import pygame
+import app
+
+# initializing pygame
+pygame.init()
+
+# screen
+screen = pygame.display.set_mode((600, 600))
+
+# background image
+background = pygame.image.load('background.png')
+
+# title and icon
+pygame.display.set_caption("Coffee Shop Database")
+icon = pygame.image.load('coffeeIcon.png')
+pygame.display.set_icon(icon)
+
+
+ def menuGUI = menu.app()
+
+
+# game loop
+running = True
+while running:
+
+    # RGB - red, green, blue
+    screen.fill((0, 0, 0))
+    # background image
+    screen.blit(background, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
